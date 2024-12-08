3b4a52683014b54fe91de26ec02df61e76dc4b15
Jeffrey Sedoeboen <j.sedoeboen@tribeagency.nl>
2024-12-08 12:27:24 +0100

    Day 8, part 1 solved

---
+++ day_8/program_part1.py
@@ -0,0 +1,46 @@
+# file = open("example_input.txt", 'r')
+file =  open("input.txt", 'r')
+lines = file.readlines()
+
+antennas = {}
+height = len(lines)
+width = len(lines[0])-1
+
+for x in range(width):
+    for y in range(height):
+        ch = lines[y][x]
+        if ch == '.': continue
+        if ch not in antennas:
+            antennas[ch] = [(x,y)]
+        else:
+            antennas[ch].append((x,y))
+
+anti_antennas = set()
+for(antenna, coords) in antennas.items():
+    for i in range(len(coords)):
+        for j in range(len(coords)):
+            if i == j: continue
+
+            antenna_coords = coords[i]
+            antenna_partner_coords = coords[j]
+
+            # Difference between the two antennas
+            dx = antenna_partner_coords[0] - antenna_coords[0]
+            dy = antenna_partner_coords[1] - antenna_coords[1]
+
+            # Calculate the two anti-antennas
+            first_anti = (antenna_partner_coords[0] + dx, antenna_partner_coords[1] + dy)
+            second_anti = (antenna_coords[0] - dx, antenna_coords[1] - dy)
+            # print(f"antenna: {antenna} {antenna_coords} {antenna_partner_coords}")
+
+            # check if in grid
+            if 0 <= first_anti[0] < width and 0 <= first_anti[1] < height:
+                anti_antennas.add(first_anti)
+
+            if 0 <= second_anti[0] < width and 0 <= second_anti[1] < height:
+                anti_antennas.add(second_anti)
+
+
+# print(f"w: {width} h: {height}")
+print(anti_antennas)
+print(len(anti_antennas))
